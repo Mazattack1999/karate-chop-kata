a1c65b44ae420bd0e24b76febc1f650b2851ab4d
Micah Zahn <micah.a.zahn@gmail.com>
2024-07-23 19:37:51 -0500

    add first failing test

--- KarateServiceTests/UnitTest1.cs
+++ KarateServiceTests/UnitTest1.cs
@@ -4,12 +4,16 @@ namespace KarateServiceTests
 {
     public class KarateTests
     {
-        public void ChopArray(int valueToLookFor, int[] valueArray)
+        [TestCase(2, new int[] {1, 2, 3}, 1)]
+        [TestCase(1, new int[] {1, 2, 3}, 0)]
+        public void ChopArray(int valueToLookFor, int[] valueArray, int expectedIndex)
         {
             var chopService = new ChopService();
             var valueList = ConvertArrayToList(valueArray);
 
             var indexOfValue = chopService.Chop(valueToLookFor, valueList);
+
+            Assert.That(indexOfValue, Is.EqualTo(expectedIndex));
         }
 
         private static List<int> ConvertArrayToList(int[] array) 
--- KarateServiceTests/obj/Debug/net8.0/KarateServiceTests.AssemblyInfo.cs
+++ KarateServiceTests/obj/Debug/net8.0/KarateServiceTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarateServiceTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d5d040886df3c86c3cc614ed81e7c6a256cf6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e556391d5a6271966ed70fafc61ebee6ddc14794")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarateServiceTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarateServiceTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
